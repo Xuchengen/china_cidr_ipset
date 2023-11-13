148a838b4d4fee86ed6693fc984411139c81cf78
徐承恩 <771247770@qq.com>
2023-11-13 19:18:46 +0800

    创建工作流

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ curl -LR -o CN-ip-cidr.txt "https://github.com/Hackl0us/GeoIP2-CN/raw/release/CN
 
 sudo ipset create $V_IPSET_NAME hash:net hashsize 2048 maxelem 1000000
 
-for item in $(cat CN-ip-cidr.txt); do
-    sudo ipset add $V_IPSET_NAME $item
-done
+#for item in $(cat CN-ip-cidr.txt); do
+#    sudo ipset add $V_IPSET_NAME $item
+#done
 
 sudo ipset save $V_IPSET_NAME > china_cidr_ipset.txt
