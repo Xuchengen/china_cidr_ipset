3fd2d1446dcd00e75dbc71faffdbf15c80db1d0e
徐承恩 <771247770@qq.com>
2023-11-13 20:03:01 +0800

    创建工作流

--- build.sh
+++ build.sh
@@ -6,8 +6,11 @@ V_IPSET_NAME=CHINA:CIDR
 sudo apt-get update
 sudo apt-get install -y ipset
 
+# 创建目录
+mkdir -p dist
+
 # 下载中国IP段数据
-curl -LR -o CN-ip-cidr.txt "https://github.com/Hackl0us/GeoIP2-CN/raw/release/CN-ip-cidr.txt"
+curl -LR -o dist/CN-ip-cidr.txt "https://github.com/Hackl0us/GeoIP2-CN/raw/release/CN-ip-cidr.txt"
 
 # 创建ipset集合
 sudo ipset create $V_IPSET_NAME hash:net hashsize 2048 maxelem 1000000
@@ -15,9 +18,6 @@ sudo ipset create $V_IPSET_NAME hash:net hashsize 2048 maxelem 1000000
 #for item in $(cat CN-ip-cidr.txt); do
 #    sudo ipset add $V_IPSET_NAME $item
 #done
-
-# 创建目录
-mkdir -p dist
 
 # 转储ipset文件
 sudo ipset save $V_IPSET_NAME > dist/china_cidr_ipset.txt
