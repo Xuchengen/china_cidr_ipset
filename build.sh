20fc7100f9c176b7149aec5a2d3563db4956b245
徐承恩 <771247770@qq.com>
2023-11-13 18:09:32 +0800

    创建工作流

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y ipset
 
 curl -LR -o CN-ip-cidr.txt "https://github.com/Hackl0us/GeoIP2-CN/raw/release/CN-ip-cidr.txt"
 
-sudo ipset create $V_IPSET_NAME hash:net hashsize 2048 maxelem 100000
+sudo ipset create $V_IPSET_NAME hash:net hashsize 2048 maxelem 1000000
 
 for item in $(cat CN-ip-cidr.txt); do
     sudo ipset add $V_IPSET_NAME $item
